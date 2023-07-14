efcdd729493772de750215aa9b5dc4b883e60587
Pradeep <pradeepmaurya1606@gmail.com>
2023-07-14 07:29:31 +0530

    adding cors to allow api in other domain

--- index.js
+++ index.js
@@ -7,7 +7,20 @@ const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
+const allowedOrigins = ['https://online-store-ecom.netlify.app/', 'https://online-store-ecom.netlify.app/']; // Add your allowed domains here
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
     res.send("Connected. api path :- /api/products")
--- models/product.js
+++ models/product.js
@@ -5,29 +5,53 @@ const productSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    company: {
+        type: String,
+        required: true,
+    },
     price: {
         type: Number,
         required: [true, "Enter Price"],
+    },
+    color: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: [String],
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        default: "shop",
+    },
+    rating: {
+        type: Number,
+        default: "5.0",
+    },
+    reviews: {
+        type: Number,
+        default: "100",
     },
     featured: {
         type: Boolean,
         default: false,
     },
-    rating: {
-        type: Number,
-        default: 4.9,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-    },
-    company: {
+    featured_Name: {
         type: String,
-        enum: {
-            values: ["apple", "samsung", "dell", "boat", "onepluse"],
-            message: `{VALUE} is not supported`,
-        },
+        default: "Online Store",
     },
+    // company: {
+    //     type: String,
+    //     enum: {
+    //         values: ["apple", "samsung", "dell", "boat", "onepluse"],
+    //         message: `{VALUE} is not supported`,
+    //     },
+    // },
 });
 
 module.exports = mongoose.model("Product", productSchema); 
