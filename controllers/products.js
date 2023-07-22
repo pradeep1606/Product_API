58341ba925f96e5fb37594389b2c374e02daa35e
Pradeep <pradeepmaurya1606@gmail.com>
2023-07-22 08:29:59 +0530

    change page limit

--- controllers/products.js
+++ controllers/products.js
@@ -6,9 +6,9 @@ const getAllProducts = async (req, res) => {
     if (company) { queryObject.company = company }
     if (featured) { queryObject.featured = featured }
     if (name) { queryObject.name = { $regex: name, $options: "p" } }
-
+    
     let apiData = Product.find(queryObject);
-
+    
     if (sort) {
         // let sortFix = sort.replace(",", " ");
         let sortFix = sort.split(",").join(" ");
@@ -19,14 +19,15 @@ const getAllProducts = async (req, res) => {
         let selectFix = select.split(",").join(" ");
         apiData = apiData.select(selectFix)
     }
-
+    
     let page = Number(req.query.page) || 1;
-    let limit = Number(req.query.limit) || 5;
+    let limit = Number(req.query.limit);
     let skip = (page - 1) * limit;
-
+    
     apiData = apiData.skip(skip).limit(limit);
-
+    
     const Products = await apiData;
+    console.log(Products);
     res.status(200).json({ Products, nbHits: Products.length });
 };
 
